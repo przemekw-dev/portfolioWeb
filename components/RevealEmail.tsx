5811c00a3f3e009efd0f1db103ae9083456f2ecb
prz3m01299 <prz3m01299@gmail.com>
2025-01-07 22:55:34 +0000

    Fix

--- components/RevealEmail.tsx
+++ components/RevealEmail.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 
 interface RevealEmailProps {
   hiddenText: string;
@@ -9,16 +11,24 @@ const RevealEmail: React.FC<RevealEmailProps> = ({
   hiddenText,
   promptText,
 }) => {
+  const [isClicked, setIsClicked] = useState(false);
+
   const decodedEmail = atob(hiddenText);
+
+  const handleClick = () => {
+    window.location.href = `mailto:${decodedEmail}`;
+    setIsClicked(true);
+  };
 
   return (
     <h3 className="text-xl">
-      <a
-        href={`mailto:${decodedEmail}`}
+      <button
+        onClick={handleClick}
         className="hover:text-accent transition-all duration-300"
+        disabled={isClicked}
       >
         {promptText}
-      </a>
+      </button>
     </h3>
   );
 };
