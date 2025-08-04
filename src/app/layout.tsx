3e3ed5b648db6fcd474076dbceee2560318acb8e
prz3m01299 <prz3m01299@gmail.com>
2025-08-04 23:20:38 +0100

    fix build issues

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import StairTransition from "../components/ui/StairTransition";
+// import StairTransition from "../components/ui/StairTransition";
 
 const funnelDisplay = Inter({
   variable: "--font-funnelDisplay",
